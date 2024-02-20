65b5af1199a8ae1a01f992430ee44be287ab8484
Abhishek Dholariya <abhishekdholariya2001@gmail.com>
2024-02-20 14:15:26 +0530

    project1

---
+++ javascript/js/project1.js
@@ -0,0 +1,21 @@
+let btn=document.querySelectorAll('.button');
+let body=document.querySelector('body');
+
+btn.forEach(function (btn){
+    console.log(btn);
+    btn.addEventListener('click',function () {
+        console.log(this)
+        if(this.id=='grey'){
+            body.style.backgroundColor='grey';
+        }else if(this.id=='red'){
+            body.style.backgroundColor='red';
+        }else if(this.id=='green'){
+            body.style.backgroundColor='#98FB98';
+        }else if(this.id=='blue'){
+            body.style.backgroundColor='#ADD8E6';
+        }
+        else{
+            body.style.backgroundColor='#fff'
+        }
+    }); 
+});
